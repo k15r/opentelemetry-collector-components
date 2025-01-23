52b467dc4e0ad71a57c661c58b12580b92fa69c4
Korbinian Stoemmer <korbinian.stoemmer@sap.com>
2025-01-23 15:00:39 +0100

    switch to existing branch

--- hack/scripts/create_branch_and_tag.sh
+++ hack/scripts/create_branch_and_tag.sh
@@ -53,7 +53,8 @@ fi
 # check if relase branch already exists on origin, otherwise create it
 echo "Checking if branch $BRANCH_NAME already exists on $REMOTE"
 if git ls-remote --exit-code --heads $REMOTE $BRANCH_NAME; then
-    echo "Branch $BRANCH_NAME already exists"
+    echo "Branch $BRANCH_NAME already exists, checking out"
+    git checkout $BRANCH_NAME
 else
     echo "Creating branch $BRANCH_NAME"
     git checkout -b $BRANCH_NAME
@@ -66,7 +67,8 @@ fi
 # check if tag already exists on origin, otherwise create it
 echo "Checking if tag $TAG_NAME already exists on $REMOTE"
 if git ls-remote --exit-code --tags $REMOTE $TAG_NAME; then
-    echo "Tag $TAG_NAME already exists"
+    echo "Tag $TAG_NAME already exists, exiting"
+    exit 1
 else
     echo "Creating tag $TAG_NAME"
     git tag $TAG_NAME
